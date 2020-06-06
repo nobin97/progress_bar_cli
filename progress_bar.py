aa99fb4dfac37812a524fd9e8e03b39bdf893cf8
nobinj <nobinj@qburst.com>
2020-06-06 19:15:46 +0530

    updated readme and code clean up

--- progress_bar.py
+++ progress_bar.py
@@ -4,32 +4,6 @@ from subprocess import call
 import time
 
 
-# A simple Progress Bar.
-# Usage:
-#   from progress_bar import ProgressBar
-#   pbr = ProgressBar()
-#   for i in range(10):
-#       pbr.progress_bar(value=i, final=10)                 // shows progress using progress bar
-#       or
-#       pbr.progress_bar(value=i, final=10, delay="random") // shows progress with a random delay
-#       or
-#       pbr.progress_bar(value=i, final=10, delay=1)        // shows progress with a 1 sec delay
-#
-# Customising Bar:
-# Default Bar length is 100 chars, you can override it by:
-#   pbr = ProgressBar(bar_length=50,   # A 50 chars long progress bar gets initialised
-#     in_progress_color="green",       # The color of the in_progress_character can be set like this
-#     incomplete_color="red",          # The color of the incomplete_character can be set like this
-#     in_progress_character="+",       # The value of the in_progress_character can be set like this
-#     incomplete_character="_",        # The value of the incomplete_character can be set like this
-#     theme_color="light-yellow")      # A theme color for title and completion info can be set like this
-# Default Title is "Progress Bar", override it by:
-#   pbr.title = "New Title"
-# By default title is shown, you can make it invisible by:
-#   pbr.show_title = False
-# By default progress bar doesn't clear the screen while updating, override it by:
-#   pbr.clear_screen = False
-
 class ProgressBar:
     theme_color = None
     clear_screen = False
